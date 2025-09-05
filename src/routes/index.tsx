fb118f04fc8234ab1eaf5a45e8fdfc29d25284e9
Harry Yu <yupakhei456@gmail.com>
2025-09-04 22:45:54 -0400

    Add basic sign out function

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,43 @@
-import { A } from "@solidjs/router";
+import { A, useNavigate } from "@solidjs/router";
+import { createEffect, Show } from "solid-js";
+import { Button } from "@/components/ui/button";
+import { showToast } from "@/components/ui/toast";
+import { authClient } from "@/lib/auth-client";
 
 export default function Home() {
+  const session = authClient.useSession();
+  const navigate = useNavigate();
+
+  createEffect(() => {
+    console.info(session().data);
+  });
+
   return (
     <main class="flex h-dvh w-dvw items-center justify-center gap-4">
-      <A href="/login">Login</A>
-      <A href="/signup">Sign up</A>
+      <Button as={A} href="/login">
+        Login
+      </Button>
+      <Button as={A} href="/signup">
+        Sign up
+      </Button>
+      <Show when={!!session().data}>
+        <Button
+          onClick={async () => {
+            const result = await authClient.signOut();
+            if (result.error) {
+              showToast({
+                title: "Log out failed",
+                description: result.error.message,
+                variant: "error",
+              });
+            } else {
+              navigate("/login");
+            }
+          }}
+        >
+          Log out
+        </Button>
+      </Show>
     </main>
   );
 }
