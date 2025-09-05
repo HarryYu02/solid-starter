bd2cfe21ecc22775a13bcc29bdcf1dfed42e866e
Harry Yu <yupakhei456@gmail.com>
2025-09-04 22:23:21 -0400

    Add basic sign up function

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,14 +1,56 @@
+import { useNavigate } from "@solidjs/router";
 import GalleryVerticalEnd from "lucide-solid/icons/gallery-vertical-end";
 import { type Component, type ComponentProps, splitProps } from "solid-js";
 import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
 import { cn } from "@/lib/utils";
 import { TextField, TextFieldInput, TextFieldLabel } from "./ui/text-field";
+import { showToast } from "./ui/toast";
 
 export const SignUpForm: Component<ComponentProps<"div">> = (props) => {
   const [, rest] = splitProps(props, ["class"]);
+  const navigate = useNavigate();
+
   return (
     <div class={cn("flex flex-col gap-6", props.class)} {...rest}>
-      <form>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          const form = e.target as HTMLFormElement;
+          const formData = new FormData(form);
+          const email = formData.get("email");
+          const password = formData.get("password");
+          const name = formData.get("name");
+          if (
+            typeof email !== "string" ||
+            typeof password !== "string" ||
+            typeof name !== "string"
+          ) {
+            return new Error("Name, email and password are required");
+          }
+          const { data, error } = await authClient.signUp.email({
+            email,
+            password,
+            name,
+            callbackURL: "/",
+          });
+          if (error) {
+            showToast({
+              title: "Sign up failed",
+              description: error.message,
+              variant: "error",
+            });
+          } else {
+            showToast({
+              title: "Sign up successfully",
+              description: `Welcome, ${data.user.name}`,
+              variant: "success",
+            });
+            form.reset();
+            navigate("/");
+          }
+        }}
+      >
         <div class="flex flex-col gap-6">
           <div class="flex flex-col items-center gap-2">
             <a href="/" class="flex flex-col items-center gap-2 font-medium">
@@ -26,6 +68,18 @@ export const SignUpForm: Component<ComponentProps<"div">> = (props) => {
             </div>
           </div>
           <div class="flex flex-col gap-6">
+            <div class="grid gap-3">
+              <TextField class="grid w-full max-w-sm items-center gap-1.5">
+                <TextFieldLabel for="name">Name</TextFieldLabel>
+                <TextFieldInput
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="John Doe"
+                  required
+                />
+              </TextField>
+            </div>
             <div class="grid gap-3">
               <TextField class="grid w-full max-w-sm items-center gap-1.5">
                 <TextFieldLabel for="email">Email</TextFieldLabel>
