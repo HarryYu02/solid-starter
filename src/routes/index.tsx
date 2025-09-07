6928fe1436012529ca2a015b5bb0fe5330e90bf0
Harry Yu <yupakhei456@gmail.com>
2025-09-06 22:57:48 -0400

    Add user login status to home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { A, useNavigate } from "@solidjs/router";
-import { Show } from "solid-js";
+import { Match, Show, Switch } from "solid-js";
 import { Button } from "@/components/ui/button";
 import { showToast } from "@/components/ui/toast";
 import { authClient } from "@/lib/auth-client";
@@ -9,31 +9,38 @@ export default function Home() {
   const navigate = useNavigate();
 
   return (
-    <main class="flex h-dvh w-dvw items-center justify-center gap-4">
-      <Button as={A} href="/login">
-        Login
-      </Button>
-      <Button as={A} href="/signup">
-        Sign up
-      </Button>
-      <Show when={!!session().data}>
-        <Button
-          onClick={async () => {
-            const result = await authClient.signOut();
-            if (result.error) {
-              showToast({
-                title: "Log out failed",
-                description: result.error.message,
-                variant: "error",
-              });
-            } else {
-              navigate("/login");
-            }
-          }}
-        >
-          Log out
+    <main class="flex h-dvh w-dvw flex-col items-center justify-center gap-4">
+      <Switch>
+        <Match when={session().isPending}>Finding user</Match>
+        <Match when={session().data}>Hello {session().data?.user.name}</Match>
+        <Match when={true}>User not logged in</Match>
+      </Switch>
+      <div class="flex gap-2">
+        <Button as={A} href="/login">
+          Login
         </Button>
-      </Show>
+        <Button as={A} href="/signup">
+          Sign up
+        </Button>
+        <Show when={!!session().data}>
+          <Button
+            onClick={async () => {
+              const result = await authClient.signOut();
+              if (result.error) {
+                showToast({
+                  title: "Log out failed",
+                  description: result.error.message,
+                  variant: "error",
+                });
+              } else {
+                navigate("/login");
+              }
+            }}
+          >
+            Log out
+          </Button>
+        </Show>
+      </div>
     </main>
   );
 }
